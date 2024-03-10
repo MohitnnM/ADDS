b79ac78b4461805049970294a85cd1683c9af0af
Mohit <a1885648@adelaide.edu.au>
2024-03-10 20:48:27 +1030

    k

--- Referee.cpp
+++ Referee.cpp
@@ -10,10 +10,16 @@ Player * Referee::refGame(Player * player1, Player * player2){
     Move* move2 = player2->makeMove();
 
     if (move1->getName() == move2->getName()) {
-        return nullptr;
+        delete move1;
+        delete move2;
+        return nullptr; // Tie
     } else if (move1->beats(move2)) {
+        delete move2;
+        delete move1; // Delete move1 after move2 since move2 is passed to the caller
         return player1;
     } else {
+        delete move1;
+        delete move2; // Delete move2 after move1 since move1 is passed to the caller
         return player2;
     }
 }
