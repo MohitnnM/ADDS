28c72bfde1dba05786a182aba1b7be9d0e227d82
Mohit <a1885648@adelaide.edu.au>
2024-03-31 16:13:36 +1030

    k

--- Finder.cpp
+++ Finder.cpp
@@ -4,14 +4,14 @@
 
 std::vector<int> Finder::findSubstrings(std::string s1, std::string s2) {
     std::vector<int> result;
-    size_t index = 0; 
+    size_t index = 0; // Initialize index to search from
 
     for (size_t i = 1; i <= s2.size(); ++i) {
         size_t found = s1.find(s2.substr(0, i), index);
         
         if (found != std::string::npos) {
             index = found + 1;
-            result.push_back(found+1);
+            result.push_back(found);
         } else {
             result.push_back(-1);
             break;
