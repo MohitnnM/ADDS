31b2d6c4b499cc0243261e3e06416eeb5d3fedce
Mohit <a1885648@adelaide.edu.au>
2024-03-31 15:48:47 +1030

    k

--- Finder.cpp
+++ Finder.cpp
@@ -1,29 +1,21 @@
 #include "Finder.h"
-#include <iostream>
+#include <string>
 using namespace std;
 
-
-vector<int> Finder::findSubstrings(string s1, string s2) {
+std::vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
     size_t index = 0;
 
-    for (size_t i = 0; i < s2.size(); i++) {
-
-        size_t found = s1.find(s2.substr(0, i + 1), index);
-
+    for (size_t i = 1; i <= s2.size(); i++) {
+        size_t found = s1.find(s2.substr(0, i), index); 
         if (found != string::npos) {
             result.push_back(found);
-            index = found + 1; 
+            index = found + 1;
         } else {
             result.push_back(-1);
-            break;
+            break; 
         }
     }
 
-    while (result.size() < s2.size()) {
-        result.push_back(-1);
-    }
-
     return result;
-
 }
